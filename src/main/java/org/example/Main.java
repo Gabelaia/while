8de757369e3478076e5a73530b8231dc5aed5e4a
Gabelaia <lela.gabelaia@gmail.com>
2024-11-12 18:46:31 +0400

    If

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,36 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = {3, 4, 5};
+        int customerAge = 6;
+        int gender = 2;
 
-        int i = 0;
-        while (i < array.length){
-            System.out.println(array[i]);
-            i++;
+        if (gender == 1) {
+            if (customerAge >= 6 && customerAge <= 18) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა სკოლის მოსწავლის ვარდისფერი ბარათი");
+            } else if (customerAge >= 19 && customerAge <= 24) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა სტუდენტის ვარდისფერი ბარათი");
+            } else if (customerAge >= 25 && customerAge <= 64) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა ბიზნეს ვარდისფერი ბარათი");
+            } else if (customerAge >= 65) {
+                System.out.println("შენ შეგიძლია გახსნა საპენსიო ვარდისფერი ბარათი");
+            } else {
+                System.out.println("6 წლამდე ასაკის ბავშვი ვერ გახსნის ბარათს");
+            }
         }
-        System.out.println("I Love Testing");
-    }
+
+        else {
+            if (customerAge >= 6 && customerAge <= 18) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა სკოლის მოსწავლის ლურჯი ბარათი");
+            } else if (customerAge >= 19 && customerAge <= 24) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა სტუდენტის ლურჯი ბარათი");
+            } else if (customerAge >= 25 && customerAge <= 64) {
+                System.out.println("გილოავ, შენ შეგიძლია გახსნა ბიზნეს ლურჯი ბარათი");
+            } else if (customerAge >= 65) {
+                System.out.println("შენ შეგიძლია გახსნა საპენსიო ლურჯი ბარათი");
+            } else {
+                System.out.println("6 წლამდე ასაკის ბავშვი ვერ გახსნის ბარათს");
+            }
+        }
+        }
+
 }
