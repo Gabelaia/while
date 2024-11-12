142cc8b182239977f8550775a5a335c904bf7dd4
Gabelaia <lela.gabelaia@gmail.com>
2024-11-12 18:32:20 +0400

    While

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {3, 4, 5};
+
+        int i = 0;
+        while (i < array.length){
+            System.out.println(array[i]);
+            i++;
+        }
+    }
+}
